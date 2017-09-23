c67878b65cb659c9424972e26ac9cbb8b9dac653
beanstown106 <nbenis106@gmail.com>
2017-09-23 16:33:19 +0800

    [2/2] HashTweaks: Fingerprint authentication vibration
    Change-Id: Ifbf2cb650083a8bac15d595f4f36cbb2d50187f2

--- src/org/hash/hashtweaks/ButtonSettings.java
+++ src/org/hash/hashtweaks/ButtonSettings.java
@@ -19,8 +19,11 @@
 package org.hash.hashtweaks;
 
 import android.content.ContentResolver;
+import android.content.Context;
+import android.hardware.fingerprint.FingerprintManager;
 import android.os.Bundle;
 import android.support.v7.preference.Preference;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
 import android.support.v7.preference.PreferenceCategory;
 import android.support.v7.preference.PreferenceScreen;
 import android.support.v14.preference.SwitchPreference;
@@ -35,7 +38,7 @@ import com.android.internal.util.hash.DeviceUtils;
 
 import org.hash.hashtweaks.preferences.SystemSettingSwitchPreference;
 
-public class ButtonSettings extends SettingsPreferenceFragment {
+public class ButtonSettings extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
 
     private static final String CATEGORY_KEYS = "button_keys";
     private static final String CATEGORY_OTHER = "button_other";
@@ -43,9 +46,12 @@ public class ButtonSettings extends SettingsPreferenceFragment {
     private static final String KEYS_SHOW_NAVBAR_KEY = "navigation_bar_show";
     private static final String KEYS_DISABLE_HW_KEY = "hardware_keys_disable";
     private static final String SYSTEM_PROXI_CHECK_ENABLED = "system_proxi_check_enabled";
+    private static final String FINGERPRINT_VIB = "fingerprint_success_vib";
 
     private SwitchPreference mEnableNavBar;
     private SwitchPreference mDisabkeHWKeys;
+    private FingerprintManager mFingerprintManager;
+    private SwitchPreference mFingerprintVib;
 
     @Override
     public int getMetricsCategory() {
@@ -90,6 +96,16 @@ public class ButtonSettings extends SettingsPreferenceFragment {
         if (!DeviceUtils.deviceSupportsProximitySensor(getActivity()) || !supportPowerButtonProxyCheck) {
             powerCategory.removePreference(proxyCheckPreference);
         }
+
+        mFingerprintManager = (FingerprintManager) getActivity().getSystemService(Context.FINGERPRINT_SERVICE);
+        mFingerprintVib = (SwitchPreference) findPreference(FINGERPRINT_VIB);
+        if (mFingerprintManager == null || !mFingerprintManager.isHardwareDetected()){
+            otherCategory.removePreference(mFingerprintVib);
+        } else {
+            mFingerprintVib.setChecked((Settings.System.getInt(getContentResolver(),
+                    Settings.System.HASH_FINGERPRINT_SUCCESS_VIB, 1) == 1));
+            mFingerprintVib.setOnPreferenceChangeListener(this);
+        }
     }
 
     @Override
@@ -106,5 +122,15 @@ public class ButtonSettings extends SettingsPreferenceFragment {
             return true;
         }
         return super.onPreferenceTreeClick(preference);
+    }
+
+     public boolean onPreferenceChange(Preference preference, Object newValue) {
+         if (preference == mFingerprintVib) {
+            boolean value = (Boolean) newValue;
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.HASH_FINGERPRINT_SUCCESS_VIB, value ? 1 : 0);
+            return true;
+        }
+        return false;
     }
 }
