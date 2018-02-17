f19668ffb21faff517c56acba4db0cc38e099114
Simon Davie <nexx@nexxdesign.co.uk>
2018-02-17 09:19:43 -0500

    [2/2] HashTweaks: BatteryMeterDrawable: add hidden option
    Change-Id: I72c6d614789dbe31ddc305a1fe95b8a9b0cbb9e7

--- src/org/hash/hashtweaks/interfacesettings/StatusbarBatterySettings.java
+++ src/org/hash/hashtweaks/interfacesettings/StatusbarBatterySettings.java
@@ -33,6 +33,7 @@ public class StatusbarBatterySettings extends SettingsPreferenceFragment impleme
     private static final String STATUS_BAR_BATTERY_STYLE = "status_bar_battery_style";
 
     private static final int STATUS_BAR_BATTERY_STYLE_TEXT = 2;
+    private static final int STATUS_BAR_BATTERY_STYLE_HIDDEN = 3;
 
     private ListPreference mStatusBarBatteryShowPercent;
     private ListPreference mStatusBarBattery;
@@ -97,7 +98,8 @@ public class StatusbarBatterySettings extends SettingsPreferenceFragment impleme
     }
 
     private void enableStatusBarBatteryDependents(int batteryIconStyle) {
-        if (batteryIconStyle == STATUS_BAR_BATTERY_STYLE_TEXT) {
+        if (batteryIconStyle == STATUS_BAR_BATTERY_STYLE_TEXT
+                || batteryIconStyle == STATUS_BAR_BATTERY_STYLE_HIDDEN) {
             mStatusBarBatteryShowPercent.setEnabled(false);
         } else {
             mStatusBarBatteryShowPercent.setEnabled(true);
